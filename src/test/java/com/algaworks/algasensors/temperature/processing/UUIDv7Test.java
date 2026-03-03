c00cd0199ad5e26759b971053023eaa1e18eba0d
Adriano <adriano.luis.machado2013@hotmail.com>
2026-03-03 07:00:42 -0300

    10. Implementacao de Microservicos da aula 10.1 ate 10.5 Ajustando a representacao do recurso REST com classe Model

---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,35 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+//import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+//import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class UUIDv7Test {
+
+    @Test
+    void shoudGenerateUUIDv7(){
+/*      UUID uuid1 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid2 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid3 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid4 = IdGenerator.generateTimeBasedUUID();
+
+        System.out.println(UUIDb7Utils.extractOffsetDateTime(uuid1));
+        System.out.println(UUIDb7Utils.extractOffsetDateTime(uuid2));
+        System.out.println(UUIDb7Utils.extractOffsetDateTime(uuid3));
+        System.out.println(UUIDb7Utils.extractOffsetDateTime(uuid4));
+*/
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+        OffsetDateTime uuidDateTime = UUIDb7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+}
